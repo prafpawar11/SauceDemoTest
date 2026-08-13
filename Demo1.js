80640c38c74788a3ff76d0a8f4281912de426c29
prafpawar11 <prafpawar11@gmail.com>
2026-08-13 07:41:45 +0530

    Tester1 have added Demo1 File code

---
+++ Demo1.js
@@ -0,0 +1,7 @@
+console.log("Demo1 Test Cases is created by Tester 1");
+
+console.log("Tester 1 Located all Object Repository and stored in pages file");
+console.log("Tester 1 Located all Object Repository and stored in pages file");
+console.log("Tester 1 Located all Object Repository and stored in pages file");
+console.log("Tester 1 Located all Object Repository and stored in pages file");
+
